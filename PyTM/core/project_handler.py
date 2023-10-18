e686e9e19661110a4cad6e8611a667ff2ffaf02e
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2023-10-18 06:33:34 +0600

    implemented project_handler functions

--- PyTM/core/project_handler.py
+++ PyTM/core/project_handler.py
@@ -1,33 +1,55 @@
-def start():
-    """Start the project"""
-    pass
+from datetime import datetime
+from PyTM.settings import PROJECT_ABORTED, PROJECT_FINISHED, PROJECT_STARTED
+from PyTM.settings import PROJECT_PAUSED
+
+def create(data, project_name):
+    """Create & Start the project
+    """
+    if data.get(project_name):
+        data[project_name]['status'] = PROJECT_STARTED
+    else:
+        data[project_name] = {
+            "tasks": [],
+            "created_at": datetime.now(),
+            "status":  PROJECT_STARTED
+        }
+    return data
 
 
-def pause():
+def pause(data, project_name):
     """Pause the project"""
-    pass
+    if data.get(project_name):
+        data[project_name]['status'] = PROJECT_PAUSED
+    return data
 
 
-def finish():
+def finish(data, project_name):
     """Finish the project"""
-    pass
+    if data.get(project_name):
+        data[project_name]['status'] = PROJECT_FINISHED
+    return data
 
 
-def summary():
+
+def summary(data, project_name):
     """Summarize the project"""
-    pass
+    return data.get(project_name, {})
 
 
-def status():
+def status(data, project_name):
     """Status of the project"""
-    pass
+    return data.get(project_name, {}).get("status", "")
 
 
-def abort():
+def abort(data, project_name):
     """Abort the project"""
-    pass
+    if data.get(project_name):
+        data.get(project_name)['status'] = PROJECT_ABORTED
+    return data
 
 
-def remove():
+def remove(data, project_name):
     """Remove the project"""
-    pass
+    if data.get(project_name):
+        del data[project_name]
+    return data
--- PyTM/settings.py
+++ PyTM/settings.py
@@ -5,4 +5,12 @@ data_filename = "data.json"
 # make data folder hidden in the home directory
 data_folder = os.path.expanduser(f"~/.{data_foldername}")
 data_filepath = os.path.join(data_folder, data_filename )
+
+
+PROJECT_STARTED = "started"
+PROJECT_STOPPED = "stopped"
+PROJECT_FINISHED = "finished"
+PROJECT_PAUSED = "paused"
+PROJECT_ABORTED = "aborted"
+
     
