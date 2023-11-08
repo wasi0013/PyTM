9a362045b359461bb0a039881349e56eebbef4ed
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2023-11-09 00:12:26 +0600

    refactored and reformatted

--- tests/core/test_data_handler.py
+++ tests/core/test_data_handler.py
@@ -1,13 +1,11 @@
-from PyTM.core.data_handler import init_data
-from PyTM.core.data_handler import load_data
-from PyTM.core.data_handler import save_data
-from PyTM.core.data_handler import update
 import json
+
+from PyTM.core import data_handler
 
 
 def test_init_data(tmpdir):
     tmp_path = tmpdir.join("pytm-test.json")
-    init_data(tmp_path)
+    data_handler.init_data(tmp_path)
     assert tmp_path.read() == "{}"
 
 
@@ -15,18 +13,18 @@ def test_load_data_exists(tmpdir):
     tmp_path = tmpdir.join("pytm-test.json")
     data = {"user": "test"}
     tmp_path.write(json.dumps(data))
-    assert load_data(tmp_path) == data
+    assert data_handler.load_data(tmp_path) == data
 
 
 def test_load_data_doesnot_exists(tmpdir):
     tmp_path = tmpdir.join("pytm-test.json")
-    assert load_data(tmp_path) == {}
+    assert data_handler.load_data(tmp_path) == {}
 
 
 def test_save_data(tmpdir):
     tmp_path = tmpdir.join("pytm-test.json")
     data = {"user": "test"}
-    save_data(data, tmp_path)
+    data_handler.save_data(data, tmp_path)
     assert tmp_path.read() == json.dumps(data)
 
 
@@ -34,5 +32,5 @@ def test_update(tmpdir):
     f = lambda d: {"user": "test"}
     tmp_path = tmpdir.join("pytm-test.json")
     tmp_path.write("[]")
-    update(f, tmp_path)
+    data_handler.update(f, tmp_path)
     assert tmp_path.read() == '{"user": "test"}'
